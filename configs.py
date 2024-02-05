b597a1d41c7a1936003b9aa1e1cec83b596c8bdb
root <hahmwj@gmail.com>
2024-02-05 15:02:40 +0900

    config

---
+++ configs.py
@@ -0,0 +1,72 @@
+# Path to the downloaded CLIP official weights.
+# See: https://github.com/openai/CLIP/blob/a9b1bf5920416aaeaec965c25dd9e8f98c864f16/clip/clip.py#L30
+CLIP_VIT_B16_PATH = '/hahmwj/video_clip/hahm_folder/ViT-B-16.pt'
+CLIP_VIT_L14_PATH = '/hahmwj/video_clip/hahm_folder/ViT-L-14.pt'
+
+# Whether cuDNN should be temporarily disable for 3D depthwise convolution.
+# For some PyTorch builds the built-in 3D depthwise convolution may be much
+# faster than the cuDNN implementation. You may experiment with your specific
+# environment to find out the optimal option.
+DWCONV3D_DISABLE_CUDNN = True
+
+# Configuration of datasets. The required fields are listed for Something-something-v2 (ssv2)
+# and Kinetics-400 (k400). Fill in the values to use the datasets, or add new datasets following
+# these examples.
+DATASETS = {
+    'ssv2': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=174,
+    ),
+    'k400': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=400,
+    ),
+    'hmdb': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=51,
+    ),
+    'ucf': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=101,
+    ),
+    'ssv2_sample': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=174,
+    ),
+    'k400_sample': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=400,
+    ),
+    'hmdb_sample': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=51,
+    ),
+    'ucf_sample': dict(
+        TRAIN_ROOT='',
+        VAL_ROOT='',
+        TRAIN_LIST='',
+        VAL_LIST='',
+        NUM_CLASSES=101,
+    ),
+}
